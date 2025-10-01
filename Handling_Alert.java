8319d3c53ba12f5107699e9f63cb5e0b924ee0a6
Code-By-Adarsh <adarshmishra70931@gmail.com>
2025-10-02 00:02:45 +0530

    Uploading updated Handling Alert file

--- Handling_Alert.java
+++ Handling_Alert.java
@@ -1,9 +1,24 @@
+package Company;
+
+import com.beust.ah.A;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.lang.classfile.instruction.ExceptionCatch;
+import java.time.Duration;
 
 public class Handling_Alert {
+    public static void wait1(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
         /*
         Alerts in Selenium: Alerts are basically popup boxes that take your focus away from the current
@@ -33,66 +48,51 @@ public class Handling_Alert {
         driver.get("https://www.selenium.dev/selenium/web/alerts.html#");
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         driver.findElement(By.id("alert")).click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         Alert alertMsg = driver.switchTo().alert();
         String alertMessage1 = driver.switchTo().alert().getText();
-        //String alertMessage25 = alertMsg.getText(); it is okay
+        //String alertMessage2 = alertMsg.getText(); it is okay
         System.out.println("Alert Message: "+alertMessage1);
         //driver.switchTo().alert().accept();  it is okay
         alertMsg.accept();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         driver.findElement(By.id("empty-alert")).click();
         Alert alertEmp = driver.switchTo().alert();
         String alertMessage2 = driver.switchTo().alert().getText();
         System.out.println("Alert Message: "+alertMessage2);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         alertEmp.accept();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         driver.findElement(By.id("prompt")).click();
         Alert alertPrompt = driver.switchTo().alert();
         String alertMessage3 = driver.switchTo().alert().getText();
         System.out.println("Alert Message: "+alertMessage3);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         alertPrompt.sendKeys("Adarsh Mishra");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
         alertPrompt.accept();
         String result = driver.findElement(By.id("text")).getText();
         System.out.println("Prompt Result: "+result);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        wait1();
+        driver.findElement(By.id("slow-alert")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Alert slowMsg = wait.until(ExpectedConditions.alertIsPresent());
+        String msg1 = slowMsg.getText();
+        System.out.println("Slow section msg: "+msg1);
+        wait1();
+        slowMsg.accept();
+        wait1();
+        driver.findElement(By.id("open-page-with-onload-alert")).click();
+        Alert newPage = driver.switchTo().alert();
+        wait1();
+        newPage.accept();
+        String msg2 = driver.findElement(By.tagName("p")).getText();
+        wait1();
+        driver.navigate().back();
+        System.out.println("New page text: "+msg2);
+        wait1();
         driver.quit();
     }
 }
