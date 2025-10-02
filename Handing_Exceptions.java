7b9065d929d1e5977456a8e73ebe9714e0fae62c
Code-By-Adarsh <adarshmishra70931@gmail.com>
2025-10-02 22:24:17 +0530

    Adding new concept on Handling Exception

---
+++ Handing_Exceptions.java
@@ -0,0 +1,52 @@
+package Company;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Handing_Exceptions {
+    public static void wait1(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        /*
+        Exception: An exception is a event that disrupts the normal flow of the program.
+        -->[Problem occurs->Create Exception->Throw exception->Handle exception]
+
+        Exception Hierarchy:
+                                            |->Checked Exception
+                              |->Exceptions->|
+        Object -> Throwable ->|             |->Unchecked Exception
+                              |          |->Virtual Machine Error
+                              |->Errors->|
+                                         |->Assertion Error
+
+
+        Checked Exception: An exception that is checked by the compiler at compilation time.This is also called
+                           compile-time exception and cannot be simply ignored the programmer should handle these
+                           exception.
+
+        Unchecked Exception: An exception that is occurred during runtime.This is also called run-time exception.And
+                             can be ignored during compilation.
+         */
+
+        System.setProperty("webdriver.chrome.driver","C:\\selenium webdriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        wait1();
+        try {
+            driver.findElement(By.id("fake")).click();
+        }catch (Exception e){
+            System.out.println("No such element is present.");
+            System.out.println("Hii");
+        }
+        System.out.println("Title of this page: "+driver.getTitle());
+        driver.quit();
+    }
+}
