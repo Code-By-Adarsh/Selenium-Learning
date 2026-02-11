d52a20f9b7229de0b10c4ea1ca0229378539de31
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-02-11 16:31:09 +0530

    Update: Listener revision file

--- MyListener.java
+++ MyListener.java
@@ -1,38 +1,56 @@
-package Company;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
 
 public class MyListener implements WebDriverListener {
-
-    @Override
-    public void beforeClick(WebElement element) {
-        System.out.println("🔵 Click hone wala hai on: " + element);
-    }
-
-    @Override
-    public void afterClick(WebElement element) {
-        System.out.println("🟢 Click ho gaya on: " + element);
-    }
-
     @Override
     public void beforeGet(WebDriver driver, String url) {
         System.out.println("🔵 Navigating to: " + url);
     }
 
     @Override
-    public void afterGet(WebDriver driver, String url) {
+    public void afterGet(WebDriver driver, String url){
         System.out.println("🟢 Navigated to: " + url);
     }
 
     @Override
-    public void beforeSendKeys(WebElement element, CharSequence... keysToSend) {
+    public void beforeClick(WebElement element){
+        System.out.println("🔵 Click hone wala hai on: " + element);
+    }
+
+    @Override
+    public void afterClick(WebElement element){
+        System.out.println("🟢 Click ho gaya on: "+element);
+    }
+
+    @Override
+    public void beforeSendKeys(WebElement element,CharSequence... keysToSend){
         System.out.println("🔵 Typing: " + String.join("", keysToSend));
     }
 
     @Override
     public void afterSendKeys(WebElement element, CharSequence... keysToSend) {
         System.out.println("🟢 Typed: " + String.join("", keysToSend));
+    }
+
+    @Override
+    public void beforeFindElement(WebDriver driver, By locator) {
+        System.out.println("🔵 Trying to find element: " + locator);
+    }
+
+    @Override
+    public void afterFindElement(WebDriver driver, By locator, WebElement element) {
+        System.out.println("🟢 Element found: " + element);
+    }
+
+    @Override
+    public void beforeQuit(WebDriver driver){
+        System.out.println("Browser is quiting");
+    }
+
+    @Override
+    public void beforeClose(WebDriver driver){
+        System.out.println("Browser is closing");
     }
 }
--- WebdriverListeners.java
+++ WebdriverListeners.java
@@ -1,12 +1,10 @@
-package Company;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 
-public class practiceOfWebDriverListener {
+public class WebdriverListeners {
     public static void wait1(){
         try {
             Thread.sleep(2000);
@@ -15,18 +13,23 @@ public class practiceOfWebDriverListener {
         }
     }
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\selenium webdriver\\chromedriver.exe");
+        /*
+        What are WebDriver Listeners?
+        Definition: A WebDriver Listener is an interface in Selenium that listens to events happening during the
+                    execution of your test.Basically, it “listens” to WebDriver actions like click, navigate,
+                    find element, etc., and allows you to perform extra actions when these events happen.
+        */
         WebDriver driver = new ChromeDriver();
-        WebDriverListener listener = new MyListener1();
+        WebDriverListener listener = new MyListener();
         EventFiringDecorator decorator = new EventFiringDecorator(listener);
         driver = decorator.decorate(driver);
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.get("https://www.flipkart.com/");
         wait1();
-        driver.findElement(By.className("Pke_EE")).sendKeys("earbuds");
+        driver.findElement(By.name("q")).sendKeys("earbuds");
         wait1();
-        driver.findElement(By.className("_2iLD__")).click();
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
         wait1();
         driver.navigate().to("https://www.amazon.in");
         wait1();
